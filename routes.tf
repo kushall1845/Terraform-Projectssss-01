6e115d69e0a608b0992453f511b0638cd560b701
kushal <kushall1845@gmai.com>
2025-09-02 23:14:00 +0530

    Added private route table to vpc-01

--- routes.tf
+++ routes.tf
@@ -13,3 +13,25 @@ resource "aws_default_route_table" "vpc-01-default-rt" {
   }
   
 }
+
+
+resource "aws_route_table" "vpc-01-private-rt" {
+  vpc_id = aws_vpc.vpc-01.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.vpc-01-nat-gw.id
+  }
+
+  
+  tags = {
+    Name = "VPC-01-Private-rt"
+  }
+}
+
+resource "aws_route_table_association" "vpc-01-private-rt-assoc" {
+  count          = length(data.aws_availability_zones.available.names)
+  subnet_id      = aws_subnet.vpc-01-private-subnets[count.index].id
+  route_table_id = aws_route_table.vpc-01-private-rt.id
+  
+}
