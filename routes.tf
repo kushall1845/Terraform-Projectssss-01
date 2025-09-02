74a9ac2d6c819fc39375d02b04d7d9fc0b05503c
kushal <kushall1845@gmai.com>
2025-09-02 23:21:34 +0530

    Added associations

--- routes.tf
+++ routes.tf
@@ -27,11 +27,15 @@ resource "aws_route_table" "vpc-01-private-rt" {
   tags = {
     Name = "VPC-01-Private-rt"
   }
+
+  depends_on = [ aws_nat_gateway.vpc-01-nat-gw ]
 }
 
 resource "aws_route_table_association" "vpc-01-private-rt-assoc" {
   count          = length(data.aws_availability_zones.available.names)
   subnet_id      = aws_subnet.vpc-01-private-subnets[count.index].id
   route_table_id = aws_route_table.vpc-01-private-rt.id
+
+  depends_on = [ aws_route_table.vpc-01-private-rt ]
   
 }
