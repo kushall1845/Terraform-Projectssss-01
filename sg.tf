be12ab684e4bafd232ff98a6700d03f844d48a8c
kushal <kushall1845@gmai.com>
2025-09-04 17:18:47 +0530

    Added public-alb sg

--- sg.tf
+++ sg.tf
@@ -20,3 +20,36 @@ resource "aws_default_security_group" "vpc-01-default-sg" {
     Name = "${var.vpc-01-name}-Default-SG"
   }
 }
+
+
+
+
+resource "aws_security_group" "public_alb_sg" {
+
+  description = "This security group is for the public ALB"
+  vpc_id      = aws_vpc.vpc-01.id
+
+  tags = {
+    Name = "Public-ALB-SG"
+  }
+
+  depends_on = [ aws_instance.vpc_01_web_servers ]
+}
+
+resource "aws_vpc_security_group_ingress_rule" "inbounc_public_alb" {
+  security_group_id = aws_security_group.public_alb_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"  # All protocols
+  from_port         = 0
+  to_port           = 0
+
+   depends_on = [ aws_security_group.public_alb_sg ]
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.public_alb_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+
+  depends_on = [ aws_security_group.public_alb_sg ]
+}
