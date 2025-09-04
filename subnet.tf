e9c71806cef7dc96e628c5ce4dfe539f6e8706dd
kushal <kushall1845@gmai.com>
2025-09-04 22:53:27 +0530

    Added private subnets to host app servers

--- subnet.tf
+++ subnet.tf
@@ -35,6 +35,26 @@ resource "aws_subnet" "vpc-01-private-subnets" {
 
 
 
+resource "aws_subnet" "vpc-01-private-subnets-appservers" {
+  count = length(data.aws_availability_zones.available.names)
+
+  vpc_id                  = aws_vpc.vpc-01.id
+  cidr_block              = cidrsubnet(var.vpc-01-cidr, 8, count.index + 20)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "${var.vpc-01-name}-private-subnet-appserver-${data.aws_availability_zones.available.names[count.index]}"
+    Type = "Private-APP-SERVER"
+  }
+}
+
+
+
+
+
+
+
 
 
 
