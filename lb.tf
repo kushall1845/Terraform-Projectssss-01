92b68afb49a1293b12eb555c7323edf0e10a443e
kushal <kushall1845@gmai.com>
2025-09-10 17:42:31 +0530

    Added internal load balancer that targets app servers

--- lb.tf
+++ lb.tf
@@ -31,5 +31,35 @@ resource "aws_lb_listener" "http" {
 
 
 
+resource "aws_lb" "internal_alb" {
+  
+  internal           = true
+  load_balancer_type = "application"
+  subnets            = [for subnet in aws_subnet.vpc-01-private-subnets-appservers : subnet.id]
+  security_groups    = [aws_security_group.internal_alb_sg.id]
+
+  tags = {
+    Name = "Internal-ALB"
+  }
+
+  depends_on = [ aws_lb_target_group_attachment.app_servers_attachment ]
+}
+
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.internal_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+
+  depends_on = [ aws_lb.internal_alb ]
+}
+
+
+
 
 
