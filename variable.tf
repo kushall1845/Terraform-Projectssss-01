a7a43c85213d86fed17bfa3505e9ac229be74038
kushal <kushall1845@gmai.com>
2025-09-10 17:50:03 +0530

    Added RDS variables

--- variable.tf
+++ variable.tf
@@ -45,3 +45,52 @@ variable "key_name" {
 
 
 
+variable "mydb_identifier" {
+  description = "The name of the RDS instance"
+  type        = string
+  default     = "mydb-instance"
+  
+}
+
+
+variable "mydb_engine" {
+  description = "The database engine to use"
+  type        = string
+  default     = "mysql"
+  
+}
+
+
+variable "mydb_instance_class" {
+  description = "The instance class of the RDS instance"
+  type        = string
+  default     = "db.t3.micro"
+
+}
+
+
+  variable "mydb_allocated_storage" {
+  description = "The allocated storage in GB"    
+    type        = number
+    default     = 20
+    }
+
+    variable "mydb_username" {    
+    description = "The master username for the database"
+    type        = string
+    default     = "dbadmin"
+    }
+
+ variable "mydb_password" {    
+    description = "The master password for the database"
+    type        = string
+    default     = "kushal123"
+    }
+
+    variable "mydb_skip_final_snapshot" {    
+    description = "Whether to skip the final snapshot"  
+    type        = bool
+    default     = true
+    }
+
+
